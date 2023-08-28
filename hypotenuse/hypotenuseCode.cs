eeaad104f2a92fc2605e24db75967992ce2bcedd
gsalinv <141323039+gsalinv@users.noreply.github.com>
2023-08-27 22:08:24 -0300

    Add files via upload

---
+++ hypotenuse/hypotenuseCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o lado A: ");
+            //user input
+            double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira o lado B: ");
+            //user input
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            double x = Math.Sqrt((Math.Pow(A, 2)) + (Math.Pow(B, 2)));
+            // x = raiz quadrada de a elevado a dois mais b elevado a dois
+
+            Console.WriteLine("A hipotenusa é " + x);
+
+            Console.ReadKey();
+        }
+    }
+}
